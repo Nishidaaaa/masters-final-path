19296da456e567ab3610f99e709d81dc9054db0a
y-nagano <n.yukito0113@gmail.com>
2023-03-22 17:23:12 +0000

    add comments

--- README.md
+++ README.md
@@ -1,14 +1,37 @@
-1. gitからclone
+# 概要
+修士論文で制作した自己教師あり学習(DINO)とkMeans,RFを組み合わせた画像分類器です．
+高解像度な画像を分割し自己教師を適用することで，計算資源上の問題をクリアし，また，判断要因となる画像領域の考察を行うことができます．
+
+mlflowで実験データを管理しています．
+mlflowはexperiment
+
+# 環境構築
+1. データの配置
+    - `git clone このリポジトリ`
+    - 画像データを`./dataset/`以下に配置
 2. Docker build
-    make docker-build
+
+    `make docker-build`
+
 3. Docker run
-    make docker-run
 
----以下コンテナの中
+
+    `make docker-run`
+
+---以下コンテナの中---
+
+
 4. mlflowのUIサーバー立ち上げ
-    nohup mlflow ui --port 5000 > /dev/null &
 
+
+    `nohup mlflow ui --port 5000 > /dev/null & `
 
 
 5. 訓練実行
-    python run_all.py 
+
+    `python run_all.py`
+
+6. 実験結果確認
+
+    [localhost:5000](http://localhost:5000)
+
--- run_all.py
+++ run_all.py
@@ -15,28 +15,68 @@ experiment_name = "test"
 
 @hydra.main(config_path="./config", config_name="run_all", version_base="1.2")
 def run_all(config: omegaconf.DictConfig) -> None:
+    """提案手法をすべて実行します．
+    各種解析結果やグラフも出力します．
+    mlflowを3段の入れ子にして実験データを管理しています．
+    これにより，訓練済みのモデルや出力結果を異なる条件で実行する際に再利用することができます．
+    また，コンフィグはHydraによって管理しています．
+
+    mlflowはexperiment(フォルダ)にrun(実験結果ファイル)を入れていくようなイメージでデータを管理します．
+    各runはrun_idという固有のidを持ちます．
+    実験データを再利用する際は，そのrun_idをコンフィグで指定してください．
+    指定されなかった場合は，ランダムにidが割り振られて，新しく訓練を行います．
+    *******3段構成ですが，再利用する際は必ず親のデータも再利用してください．******
+
+    Args:
+        config (omegaconf.DictConfig): 一連の実行に関するコンフィグ. デコレータによって自動で渡されます．
+    """
     experiment = mlflow.set_experiment(experiment_name)
     id_logger = IDLogger()
+
     with mlflow.start_run(run_id=config.extract_features_id, experiment_id=experiment.experiment_id):
+        """mlflow1段目
+        パッチ画像から自己教師で特徴を抽出するDINOを訓練します．
+        モデルや抽出した特徴量等...は保存されます．
+        もしすでに保存されていた場合は読み込みます．
+        """
         id_logger.log("extract_features_id")
         extract_features_config = recover_and_log_config(config.extract_features)
 
         data_module, model, features = extract_features(experiment_name=experiment_name, **extract_features_config)
 
+        # パッチレベルのデータ(サンプル数大)
         patch_labels = data_module.patch_labels
         patch_classes = data_module.patch_classes
-        cumulative_sums = data_module.cumulative_sums
+
+        # 元画像レベルのデータ(サンプル数小)
         original_labels = data_module.original_labels
         original_classes = data_module.original_classes
         original_fullpaths = data_module.original_fullpaths
+        # 各画像から何枚パッチを生成したかを累積和にしたもの
+        # 例 画像3枚でそれぞれ30枚40枚30枚のパッチを切り出した場合
+        #    cumulative_sums=[30, 70, 100]
+        cumulative_sums = data_module.cumulative_sums
 
         with mlflow.start_run(run_id=config.reduce_dimension_id, nested=True):
+            """mlflow2段目
+            1段目で抽出した特徴量を次元圧縮します．
+            次元圧縮に用いたモデルや次元圧縮後のデータは保存されます．
+            もしすでに保存されていた場合は読み込みます．
+            """
             id_logger.log("reduce_dimension_id")
             reduce_dimension_config = recover_and_log_config(config.reduce_dimension)
 
             reduced_features = reduce_dimension(features=features, **reduce_dimension_config)
 
             with mlflow.start_run(run_id=config.clusterize_and_classify_id, nested=True):
+                """mlflow3段目
+                2段目で次元圧縮したデータから元画像のラベルを推論するようなモデルを訓練します．
+                kmeansでクラスタリング->ヒストグラムに変形->RandomForestで推論という流れです．
+                モデルや中間データは保存されます．
+                もしすでに保存されていた場合は読み込みます．
+                また，結果から様々なグラフを生成します．
+                グラフは実行ごとに再生成されます．
+                """
                 id_logger.log("clusterize_and_classify_id")
                 clusterize_and_classify_config = recover_and_log_config(config.clusterize_and_classify)
                 n_clusters = clusterize_and_classify_config.kmeans_args.n_clusters
